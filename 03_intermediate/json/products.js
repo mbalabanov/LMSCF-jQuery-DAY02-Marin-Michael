bf5c50b7c6c52cda98b875b0dc8ee53a6b5352fd
Marin Balabanov <marincomics@gmail.com>
2020-02-11 11:30:00 +0100

    Added fake jewelry data

---
+++ 03_intermediate/json/products.js
@@ -0,0 +1,78 @@
+// This file parses the all products from the JSON data and stores them in a variable.
+
+let productdata = JSON.parse(`{
+    "jewelry": [
+       {
+         "id": "1",
+         "name": "Collette Z-Sterling-Silver with Rhodium-Plated Clear Round Cubic Zirconia Bezel Halo Dangle Earrings",
+         "image": "img/01_earrings.jpg",
+         "amount": "12",
+         "original_price": "300",
+         "price": "200"
+       },
+       {
+         "id": "2",
+         "name": "Yellow Gold-and-Black Hills 10 carat Gold-Diamond Heart Pendant",
+         "image": "img/02_pendant.jpg",
+         "amount": "16",
+         "original_price": "",
+         "price": "150"
+       },
+       {
+         "id": "3",
+         "name": "Black Hills Gold-on-Silver Open-Heart Pendant ",
+         "image": "img/03_pendant.jpg",
+         "amount": "32",
+         "original_price": "180",
+         "price": "160"
+       },
+       {
+         "id": "4",
+         "name": "Yellow 18 carat Gold-plated Cubic Zirconia Solitaire Engagement Ring",
+         "image": "img/04_ring.jpg",
+         "amount": "2",
+         "original_price": "",
+         "price": "200"
+       },
+       {
+         "id": "5",
+         "name": "Glitzy Rocks Sterling Silver London Blue-Topaz Braided Link Bracelet",
+         "image": "img/05_bracelet.jpg",
+         "amount": "22",
+         "original_price": "340",
+         "price": "300"
+       },
+       {
+         "id": "6",
+         "name": "Handmade Gold-Accent Sterling-Silver Wishes Bracelet",
+         "image": "img/06_bracelet.jpg",
+         "amount": "2",
+         "original_price": "",
+         "price": "360"
+       },
+       {
+         "id": "7",
+         "name": "Moissanite by Charles Colvard 14 carat Gold Round Solitaire Ring",
+         "image": "img/07_ring.jpg",
+         "amount": "4",
+         "original_price": "",
+         "price": "240"
+       },
+       {
+         "id": "8",
+         "name": "Sterling Silver Cubic Zirconia Solitaire Engagement Ring",
+         "image": "img/08_ring.jpg",
+         "amount": "6",
+         "original_price": "320",
+         "price": "220"
+       },
+       {
+         "id": "9",
+         "name": "Sterling Silver Round Solitaire Zirconia Stud Earrings",
+         "image": "img/09_earrings.jpg",
+         "amount": "18",
+         "original_price": "",
+         "price": "380"
+       }
+    ]
+ }`)
