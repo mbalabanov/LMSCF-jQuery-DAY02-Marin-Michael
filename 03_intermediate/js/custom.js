50313661c6ce34d7c925e60aa3a96ffaf3f87134
Marin Balabanov <marincomics@gmail.com>
2020-02-11 16:12:40 +0100

    Added shopping cart iteration

---
+++ 03_intermediate/js/custom.js
@@ -0,0 +1,59 @@
+let ordersarray = [];
+
+for (let i in productdata.jewelry) {
+	$('#allproducts').append(`
+		<div class="productcontainer">
+			<div class="imagecontainer">
+				<img src="${productdata.jewelry[i].image}" alt="${productdata.jewelry[i].name}" class="present">
+			</div>
+			<div class="productinfo">
+				<p class="productname">${productdata.jewelry[i].name}</p>
+				<p class="price"><strong>$${productdata.jewelry[i].price}</strong><span class="originalprice"></span></p>
+				<button id="${productdata.jewelry[i].id}" class="orderbutton">&#128722;&nbsp;Add to cart</button>
+			</div>
+		</div>
+	`)
+}
+
+ $('.orderbutton').on('click', function () {
+ 	console.log(this.id);
+	ordersarray.push(productdata.jewelry[this.id]);
+	console.table(ordersarray);
+ });
+
+ $('#shoppingcart').on('click', function() {
+ 	
+ 	let orderitem = '';
+ 	let cartContentDiv = '';
+
+	for (let i in ordersarray) {
+
+		$('#orders').append(`
+				<table id="ordertable">
+					<tr>
+						<td class="imagecolumn"><img src="${ordersarray[i].image}" alt="${ordersarray[i].name}"></td>
+						<td class="namecolumn">
+							<div class="truncatedtext">${ordersarray[i].name}</div>
+							<div class="productprice"><strong>$${ordersarray[i].price}</strong></div>
+						</td>
+						<td class="add"><button type="button">+</button></td>
+						<td class="amountcolumn">1</td>
+						<td class="substract"><button type="button">-</button></td>
+						<td class="pricesubtotal">$340</td>
+					</tr>
+				</table>
+			</div>
+		`);
+
+	}
+
+
+});
+
+function generateCart() {
+
+}
+
+function cartContent() {
+
+};
--- 03_intermediate/json/products.js
+++ 03_intermediate/json/products.js
@@ -6,7 +6,8 @@ let productdata = JSON.parse(`{
          "id": "1",
          "name": "Collette Sterling-Silver Zirconia Bezel Halo Dangle Earrings",
          "image": "img/01_earrings.jpg",
-         "amount": "12",
+         "amount_stock": "12",
+         "amount_ordered": "",
          "original_price": "300",
          "price": "200"
        },
@@ -14,7 +15,8 @@ let productdata = JSON.parse(`{
          "id": "2",
          "name": "Yellow Gold-and-Black Hills 10 carat Gold-Diamond Heart Pendant",
          "image": "img/02_pendant.jpg",
-         "amount": "16",
+         "amount_stock": "8",
+         "amount_ordered": "",
          "original_price": "",
          "price": "150"
        },
@@ -22,7 +24,8 @@ let productdata = JSON.parse(`{
          "id": "3",
          "name": "Black Hills Gold-on-Silver Open-Heart Pendant ",
          "image": "img/03_pendant.jpg",
-         "amount": "32",
+         "amount_stock": "32",
+         "amount_ordered": "",
          "original_price": "180",
          "price": "160"
        },
@@ -30,7 +33,8 @@ let productdata = JSON.parse(`{
          "id": "4",
          "name": "Yellow 18 carat Gold-plated Cubic Zirconia Solitaire Engagement Ring",
          "image": "img/04_ring.jpg",
-         "amount": "2",
+         "amount_stock": "2",
+         "amount_ordered": "",
          "original_price": "",
          "price": "200"
        },
@@ -38,7 +42,8 @@ let productdata = JSON.parse(`{
          "id": "5",
          "name": "Glitzy Rocks Sterling Silver London Blue-Topaz Braided Link Bracelet",
          "image": "img/05_bracelet.jpg",
-         "amount": "22",
+         "amount_stock": "22",
+         "amount_ordered": "",
          "original_price": "340",
          "price": "300"
        },
@@ -46,7 +51,8 @@ let productdata = JSON.parse(`{
          "id": "6",
          "name": "Handmade Gold-Accent Sterling-Silver Wishes Bracelet",
          "image": "img/06_bracelet.jpg",
-         "amount": "2",
+         "amount_stock": "2",
+         "amount_ordered": "",
          "original_price": "",
          "price": "360"
        },
@@ -54,7 +60,8 @@ let productdata = JSON.parse(`{
          "id": "7",
          "name": "Moissanite by Charles Colvard 14 carat Gold Round Solitaire Ring",
          "image": "img/07_ring.jpg",
-         "amount": "4",
+         "amount_stock": "4",
+         "amount_ordered": "",
          "original_price": "",
          "price": "240"
        },
@@ -62,7 +69,8 @@ let productdata = JSON.parse(`{
          "id": "8",
          "name": "Sterling Silver Cubic Zirconia Solitaire Engagement Ring",
          "image": "img/08_ring.jpg",
-         "amount": "6",
+         "amount_stock": "6",
+         "amount_ordered": "",
          "original_price": "320",
          "price": "220"
        },
@@ -70,7 +78,8 @@ let productdata = JSON.parse(`{
          "id": "9",
          "name": "Sterling Silver Round Solitaire Zirconia Stud Earrings",
          "image": "img/09_earrings.jpg",
-         "amount": "18",
+         "amount_stock": "18",
+         "amount_ordered": "",
          "original_price": "",
          "price": "380"
        }
