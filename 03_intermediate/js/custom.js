0e650640af450b87d22b2819213b9a740d48c262
Marin Balabanov <marincomics@gmail.com>
2020-02-12 01:12:49 +0100

    Adjustments to styling and tweaks to amounts functionalities

--- 03_intermediate/js/custom.js
+++ 03_intermediate/js/custom.js
@@ -4,6 +4,8 @@ let cartContent = '';
 
 function initializeShop() {
 
+	$('#cartabsolute').hide();
+
 	for (let i in productdata.jewelry) {
 		$('#allproducts').append(`
 			<div class="productcontainer">
@@ -27,30 +29,31 @@ function initializeShop() {
 
 function addItemToCart() {
 	let item = this.id;
+	productdata.jewelry[item].amount_cart = '1';
 	let itemOrder = productdata.jewelry[this.id];
 	
-	$('#orders').hide();
+	$('#cartabsolute').hide();
 
 	if (jQuery.inArray(item, ordersClick) == '-1') {
 		ordersClick.push(item);
 		ordersArray.push(itemOrder);
 		$('#cartamount').replaceWith('<span id="cartamount">' + ordersArray.length + '</span>');
 		$(this).replaceWith('<button class="addedButton">Added to cart</button>');
-		$('.addedButton').click(function() { $('#orders').hide(); });
+		$('.addedButton').click(function() { $('#cartabsolute').hide(); });
 	}
 }
 
 function generateOrderTable() {
-	$('#orders').hide();
+	$('#cartabsolute').hide();
 	$('#orders').replaceWith('<div id="orders"></div>');
 
 	if (ordersArray.length == 0) {
 		$('#orders').replaceWith('<div id="orders"><p align="center">Your shopping cart is empty.</p></div>');
-		$('#orders').show();
+		$('#cartabsolute').show();
 	};
 
 	for (let i in ordersArray) {
-		let currentAmountPrice = ordersArray[i].amount_ordered * ordersArray[i].price;
+		let currentAmountPrice = ordersArray[i].amount_cart * ordersArray[i].price;
 
 		$('#orders').append(`
 			<div id="orders">
@@ -59,9 +62,9 @@ function generateOrderTable() {
 						<td class="imagecolumn"><img src="${ordersArray[i].image}" alt="${ordersArray[i].name}"></td>
 						<td class="namecolumn">
 							<div class="truncatedtext">${ordersArray[i].name}</div>
-							<div class="productprice"><strong>$${ordersArray[i].price}</strong> (${ordersArray[i].amount_stock} in stock)</div>
+							<div class="productprice"><strong>$${ordersArray[i].price} pp</strong> (${ordersArray[i].amount_stock} in stock)</div>
 						</td>
-						<td class="amountcolumn"><input type="number" maxlength="2" min="0" max="${ordersArray[i].amount_stock}" value="${ordersArray[i].amount_ordered}" class="itemAmountField" id="ia${ordersArray[i].id}"></td>
+						<td class="amountcolumn"><input type="number" maxlength="2" min="0" max="${ordersArray[i].amount_stock}" value="${ordersArray[i].amount_cart}" class="itemAmountField" id="ia${ordersArray[i].id}"></td>
 						<td class="itemPriceSubtotal"><span id="itp${ordersArray[i].id}">$${currentAmountPrice}</span></td>
 					</tr>
 				</table>
@@ -70,14 +73,14 @@ function generateOrderTable() {
 
 	};
 
-	$('#orders').show();
+	$('#cartabsolute').show();
 
 	$('.itemAmountField').change(function() {
 		let tempID = $(this).attr('id');
 		let tempValue = $(this).val()
 		tempID = tempID.substring(2);
 		let itemIndex = productdata.jewelry.findIndex((obj => obj.id == tempID));
-		productdata.jewelry[itemIndex].amount_ordered = tempValue;
+		productdata.jewelry[itemIndex].amount_cart = tempValue;
 		generateOrderTable();
 	});	
 };
--- 03_intermediate/json/products.js
+++ 03_intermediate/json/products.js
@@ -7,7 +7,7 @@ let productdata = JSON.parse(`{
          "name": "Collette Sterling-Silver Zirconia Bezel Halo Dangle Earrings",
          "image": "img/01_earrings.jpg",
          "amount_stock": "8",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "300",
          "price": "200"
        },
@@ -16,7 +16,7 @@ let productdata = JSON.parse(`{
          "name": "Yellow Gold-and-Black Hills 10 carat Gold-Diamond Heart Pendant",
          "image": "img/02_pendant.jpg",
          "amount_stock": "6",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "",
          "price": "150"
        },
@@ -25,7 +25,7 @@ let productdata = JSON.parse(`{
          "name": "Black Hills Gold-on-Silver Open-Heart Pendant ",
          "image": "img/03_pendant.jpg",
          "amount_stock": "3",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "180",
          "price": "160"
        },
@@ -34,7 +34,7 @@ let productdata = JSON.parse(`{
          "name": "Yellow 18 carat Gold-plated Cubic Zirconia Solitaire Engagement Ring",
          "image": "img/04_ring.jpg",
          "amount_stock": "2",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "",
          "price": "200"
        },
@@ -43,7 +43,7 @@ let productdata = JSON.parse(`{
          "name": "Glitzy Rocks Sterling Silver London Blue-Topaz Braided Link Bracelet",
          "image": "img/05_bracelet.jpg",
          "amount_stock": "6",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "340",
          "price": "300"
        },
@@ -52,7 +52,7 @@ let productdata = JSON.parse(`{
          "name": "Handmade Gold-Accent Sterling-Silver Wishes Bracelet",
          "image": "img/06_bracelet.jpg",
          "amount_stock": "2",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "",
          "price": "360"
        },
@@ -61,7 +61,7 @@ let productdata = JSON.parse(`{
          "name": "Moissanite by Charles Colvard 14 carat Gold Round Solitaire Ring",
          "image": "img/07_ring.jpg",
          "amount_stock": "4",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "",
          "price": "240"
        },
@@ -70,7 +70,7 @@ let productdata = JSON.parse(`{
          "name": "Sterling Silver Cubic Zirconia Solitaire Engagement Ring",
          "image": "img/08_ring.jpg",
          "amount_stock": "6",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "320",
          "price": "220"
        },
@@ -79,7 +79,7 @@ let productdata = JSON.parse(`{
          "name": "Sterling Silver Round Solitaire Zirconia Stud Earrings",
          "image": "img/09_earrings.jpg",
          "amount_stock": "11",
-         "amount_ordered": "1",
+         "amount_cart": "0",
          "original_price": "",
          "price": "380"
        }
